2169a6c8f1520e526f33c93fa2338954e25d9574
MouYu <1030725645@qq.com>
2020-04-06 16:15:27 +0800

    修改域名

--- src/components/DataVisualization/RelevanceExplore.vue
+++ src/components/DataVisualization/RelevanceExplore.vue
@@ -212,7 +212,7 @@ export default {
 
             myChart1.setOption(option1);
 
-            Axios.get("http://zomboost.dcts.top:5555/data/demo6.json").then((res)=>{
+            Axios.get("http://zomboost.dcts.top/data/demo6.json").then((res)=>{
                 var myChart2 = this.$echarts.init(document.getElementById('demo2'), "light");
                 var option2 = {
                     color: color,
